58f8e8ecf301a72d478f8401fd527c9173732c8c
Mark2518 <marcosdiazsanchez@uma.es>
2024-11-23 14:21:42 +0100

    ARREGLADO PRODUCTOS, COPIAR RESTO RUMBERAAAAAAAA

---
+++ pcb/src/main/java/es/pcb/pcbgrupo16/Repository/ContenidoRepository.java
@@ -0,0 +1,7 @@
+package es.pcb.pcbgrupo16.Repository;
+
+import es.pcb.pcbgrupo16.Entities.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContenidoRepository extends JpaRepository<Contenido, Long> {
+}
